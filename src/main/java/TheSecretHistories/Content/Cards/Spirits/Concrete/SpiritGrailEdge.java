de4aa617ac41438caca8d713f57edd4d2ac4f977
socks1024 <3619907017@qq.com>
2025-07-22 22:43:26 +0800

    SpiritGrailEdge

--- src/main/java/TheSecretHistories/Content/Cards/Spirits/Concrete/SpiritGrailEdge.java
+++ src/main/java/TheSecretHistories/Content/Cards/Spirits/Concrete/SpiritGrailEdge.java
@@ -4,11 +4,22 @@ import TheSecretHistories.Content.Cards.Spirits.AbstractSpirit;
 import TheSecretHistories.Content.Powers.UniqueCards.Spirit_GrailE_EdgePower;
 import TheSecretHistories.Utils.PrincipleUtils;
 import TheSecretHistories.Utils.StringUtils;
+import com.badlogic.gdx.Gdx;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.SpawnMonsterAction;
+import com.megacrit.cardcrawl.actions.common.SuicideAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.monsters.beyond.*;
+import com.megacrit.cardcrawl.monsters.city.*;
+import com.megacrit.cardcrawl.monsters.ending.CorruptHeart;
+import com.megacrit.cardcrawl.monsters.exordium.*;
 import com.megacrit.cardcrawl.ui.panels.EnergyPanel;
+
+import java.util.Objects;
+import java.util.Random;
 
 import static TheSecretHistories.Content.Characters.TheSeeker.PlayerTagEnum.*;
 
@@ -17,7 +28,7 @@ public class SpiritGrailEdge extends AbstractSpirit {
     public static final String ID = StringUtils.MakeID(SpiritGrailEdge.class.getSimpleName());
 
     private static final String IMG_NAME = "spirit_graile_edge";
-    private static final int COST = -1;
+    private static final int COST = 0;
     private static final CardType TYPE = CardType.ATTACK;
     private static final CardTarget TARGET = CardTarget.ENEMY;
     private static final PrincipleUtils.ReducePrincipleInfo[] INFOS = new PrincipleUtils.ReducePrincipleInfo[]{
@@ -29,27 +40,88 @@ public class SpiritGrailEdge extends AbstractSpirit {
     public SpiritGrailEdge() {
         super(ID, IMG_NAME, COST, TYPE, TARGET, INFOS);
         this.exhaust = true;
-        this.baseMagicNumber = this.magicNumber = 0;
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
 
-        //addToBot(new UseXAction(p, this.freeToPlayOnce));
-        int energy = AbstractDungeon.actionManager.cardQueue.get(0).energyOnUse;
-        if (this.upgraded) energy += 1;
-        if (p.hasRelic("Chemical X")) {
-            energy += 2;
-            p.getRelic("Chemical X").flash();
-        }
-        if (energy > 0) {
+        addToBot(new SuicideAction(m, false));
 
-            addToBot(new ApplyPowerAction(m, p, new Spirit_GrailE_EdgePower(m, energy), energy));
+        addToBot(new SpawnMonsterAction(GetRandomMonster(m), false));
+    }
+
+    private AbstractMonster GetRandomMonster(AbstractMonster lastMonster) {
+
+        float x = (lastMonster.drawX - Settings.WIDTH * 0.75F) / Settings.xScale;
+        float y = (lastMonster.drawY - AbstractDungeon.floorY) / Settings.yScale;
+
+        Random r = new Random();
+
+        AbstractMonster monster;
+
+        float f = new Random().nextFloat();
+
+        if (f < 0.005f) {
+            monster = new CorruptHeart();
+        } else if (f < 0.2f) {
+            AbstractMonster[] ms = new AbstractMonster[] {
+                    new Spiker(x,y),
+                    new Repulsor(x,y),
+                    new Exploder(x,y),
+                    new OrbWalker(x,y),
+                    new Transient(),
+                    new Nemesis(),
+                    new Deca(),
+                    new Donu(),
+            };
+
+            monster = ms[r.nextInt(ms.length)];
+        } else if (f < 0.5f) {
+            AbstractMonster[] ms = new AbstractMonster[] {
+                    new SphericGuardian(x,y),
+                    new Looter(x,y),
+                    new Mugger(x,y),
+                    new Byrd(x,y),
+                    new Byrd(x,y),
+                    new Byrd(x,y),
+                    new SnakePlant(x,y),
+                    new Taskmaster(x,y),
+                    new SlaverRed(x,y),
+                    new SlaverBlue(x,y),
+                    new BronzeAutomaton(),
+            };
+
+            monster = ms[r.nextInt(ms.length)];
+        } else {
+            AbstractMonster[] ms = new AbstractMonster[] {
+                    new Cultist(x,y),
+                    new JawWorm(x,y),
+                    new GremlinFat(x,y),
+                    new GremlinWizard(x,y),
+                    new GremlinNob(x,y),
+                    new GremlinWarrior(x,y),
+                    new GremlinThief(x,y),
+                    new GremlinTsundere(x,y),
+                    new TheGuardian(),
+            };
+
+            monster = ms[r.nextInt(ms.length)];
         }
-        p.energy.use(EnergyPanel.totalCount);
+
+        if (Objects.equals(monster.name, lastMonster.name)) {
+            monster = GetRandomMonster(lastMonster);
+        }
+
+        if (this.upgraded) {
+            float ratio = (float) lastMonster.currentHealth / lastMonster.maxHealth;
+            monster.currentHealth = (int) (monster.maxHealth * ratio);
+            monster.healthBarUpdatedEvent();
+        }
+
+        return monster;
     }
 
     public void OnUpgrade(int timesUpgraded) {
-        upgradeMagicNumber(3);
+
     }
 }
