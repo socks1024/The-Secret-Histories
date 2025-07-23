c4f0c4b50d38b4bfa3b48225b7de3ad358f7ebcf
socks1024 <3619907017@qq.com>
2025-07-23 22:11:19 +0800

    sth

--- src/main/java/TheSecretHistories/Content/Powers/UniqueCards/FollowerMothYPower.java
+++ src/main/java/TheSecretHistories/Content/Powers/UniqueCards/FollowerMothYPower.java
@@ -8,7 +8,7 @@ public class FollowerMothYPower extends TemplateCustomPower {
 
     public static final String POWER_ID = StringUtils.MakeID(FollowerMothYPower.class.getSimpleName());
 
-    private static final String IMG_NAME = "ysabet";
+    private static final String IMG_NAME = "ysabet_a";
 
     public FollowerMothYPower(AbstractCreature owner, int amount) {
         super(POWER_ID, IMG_NAME, owner, amount);
