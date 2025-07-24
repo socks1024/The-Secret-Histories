b3df3f9037aabb4ffed7ddb59bb495fefe54b132
socks1024 <3619907017@qq.com>
2025-07-24 11:56:21 +0800

    sth

---
+++ src/main/java/TheSecretHistories/Content/Actions/UniqueCards/IngredientForgeDAction.java
@@ -0,0 +1,63 @@
+package TheSecretHistories.Content.Actions.UniqueCards;
+
+import TheSecretHistories.Content.Powers.Principles.Lantern;
+import TheSecretHistories.Utils.DebugUtils;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.GameActionManager;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.common.ExhaustAction;
+import com.megacrit.cardcrawl.actions.utility.WaitAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.UIStrings;
+
+public class IngredientForgeDAction extends AbstractGameAction {
+
+    private static final UIStrings uiStrings = CardCrawlGame.languagePack.getUIString("GamblingChipAction");
+    public static final String[] TEXT = uiStrings.TEXT;
+    private final boolean upgraded;
+
+    public IngredientForgeDAction(AbstractCreature source, boolean upgraded) {
+        setValues(AbstractDungeon.player, source, -1);
+        this.actionType = ActionType.CARD_MANIPULATION;
+        this.upgraded = upgraded;
+    }
+
+    @Override
+    public void update() {
+        if (this.duration == 0.5F) {
+            AbstractDungeon.handCardSelectScreen.open(TEXT[1], 99, true, true);
+            this.addToBot(new WaitAction(0.25F));
+            this.tickDuration();
+        } else {
+            if (!AbstractDungeon.handCardSelectScreen.wereCardsRetrieved) {
+                if (!AbstractDungeon.handCardSelectScreen.selectedCards.group.isEmpty()) {
+
+                    int count = AbstractDungeon.handCardSelectScreen.selectedCards.group.size();
+
+                    this.addToTop(new DrawCardAction(count));
+
+                    if (upgraded) {
+                        for (AbstractCard c : AbstractDungeon.handCardSelectScreen.selectedCards.group) {
+                            AbstractDungeon.player.hand.moveToDiscardPile(c);
+                            GameActionManager.incrementDiscard(false);
+                            c.triggerOnManualDiscard();
+                        }
+                    } else {
+                        for (AbstractCard c : AbstractDungeon.handCardSelectScreen.selectedCards.group) {
+                            AbstractDungeon.player.hand.moveToExhaustPile(c);
+                        }
+                        CardCrawlGame.dungeon.checkForPactAchievement();
+                    }
+                }
+
+                AbstractDungeon.handCardSelectScreen.wereCardsRetrieved = true;
+            }
+
+            this.tickDuration();
+        }
+    }
+}
--- src/main/java/TheSecretHistories/Content/Cards/FITIF/Ingredients/Forge/IngredientForgeD.java
+++ src/main/java/TheSecretHistories/Content/Cards/FITIF/Ingredients/Forge/IngredientForgeD.java
@@ -1,8 +1,10 @@
 package TheSecretHistories.Content.Cards.FITIF.Ingredients.Forge;
 
 import TheSecretHistories.Content.Actions.UniqueCards.ExhaustByTagGainBlockAction;
+import TheSecretHistories.Content.Actions.UniqueCards.IngredientForgeDAction;
 import TheSecretHistories.Content.Cards.FITIF.Ingredients.AbstractIngredient;
 import TheSecretHistories.Utils.StringUtils;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
@@ -13,25 +15,30 @@ public class IngredientForgeD extends AbstractIngredient {
     public static final String ID = StringUtils.MakeID(IngredientForgeD.class.getSimpleName());
     private static final AbstractCard.CardTags PRINCIPLE_TAG = FORGE;
     private static final String IMG_NAME = "ingredientforged";
-    private static final int COST = 1;
+    private static final int COST = 0;
     private static final AbstractCard.CardType TYPE = CardType.SKILL;
     private static final AbstractCard.CardRarity RARITY = AbstractCard.CardRarity.UNCOMMON;
     private static final AbstractCard.CardTarget TARGET = CardTarget.SELF;
 
     public IngredientForgeD() {
         super(ID, IMG_NAME, COST, TYPE, RARITY, TARGET, PRINCIPLE_TAG);
-        this.block = this.baseBlock = 7;
+        this.exhaust = true;
+//        this.block = this.baseBlock = 7;
     }
 
     @Override
     protected void OnUpgrade(int timesUpgraded) {
-        upgradeBlock(2);
+        this.exhaust = false;
     }
+
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         super.use(p, m);
-        addToBot(new ExhaustByTagGainBlockAction(p, INGREDIENT, this.block));
-        addToBot(new ExhaustByTagGainBlockAction(p, TOOL, this.block));
+
+        addToBot(new IngredientForgeDAction(p, upgraded));
+
+//        addToBot(new ExhaustByTagGainBlockAction(p, INGREDIENT, this.block));
+//        addToBot(new ExhaustByTagGainBlockAction(p, TOOL, this.block));
     }
 
 }
