bb3bf35fceb1f0798aa79c980777d399f8800d98
blackrabbit <3960419812@qq.com>
2025-07-26 23:38:24 +0800

    1

--- src/main/java/TheSecretHistories/Content/Cards/FITIF/Ingredients/Moth/IngredientMothB.java
+++ src/main/java/TheSecretHistories/Content/Cards/FITIF/Ingredients/Moth/IngredientMothB.java
@@ -26,7 +26,7 @@ public class IngredientMothB extends AbstractIngredient {
 
     private static final String IMG_NAME = "ingredientmothb";
     private static final int COST = 1;
-    private static final CardType TYPE = CardType.ATTACK;
+    private static final CardType TYPE = CardType.SKILL;
     private static final CardRarity RARITY = CardRarity.COMMON;
     private static final CardTarget TARGET = CardTarget.SELF;
 
