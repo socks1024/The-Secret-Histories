f957a8d6f336f5eb442f3407b87eb1f50319c720
socks1024 <3619907017@qq.com>
2025-07-28 01:46:19 +0800

    sth

--- src/main/java/TheSecretHistories/Content/Powers/UniqueCards/IngredientLanternFPower.java
+++ src/main/java/TheSecretHistories/Content/Powers/UniqueCards/IngredientLanternFPower.java
@@ -8,11 +8,18 @@ import com.megacrit.cardcrawl.core.AbstractCreature;
 public class IngredientLanternFPower extends TemplateCustomPower {
     public static final String POWER_ID = StringUtils.MakeID(IngredientLanternFPower.class.getSimpleName());
     private static final String IMG_NAME = "ingredientlanternf";
+
     public IngredientLanternFPower(AbstractCreature owner, int amount) {
         super(POWER_ID, IMG_NAME, owner, amount);
         this.type = PowerType.BUFF;
     }
     public void onInitialApplication() {
-        BaseMod.MAX_HAND_SIZE += 999;
+        BaseMod.MAX_HAND_SIZE = 999;
+    }
+
+    @Override
+    public void onRemove() {
+        super.onRemove();
+        BaseMod.MAX_HAND_SIZE = 10;
     }
 }
