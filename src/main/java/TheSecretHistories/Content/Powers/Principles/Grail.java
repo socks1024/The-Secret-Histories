01ce74492fa337f9109b4328623da31255eff189
socks1024 <3619907017@qq.com>
2025-07-30 06:52:08 +0800

    sth

--- src/main/java/TheSecretHistories/Content/Powers/Principles/Grail.java
+++ src/main/java/TheSecretHistories/Content/Powers/Principles/Grail.java
@@ -1,5 +1,7 @@
 package TheSecretHistories.Content.Powers.Principles;
 
+import TheSecretHistories.Content.Actions.Principle.ConsumePrinciple.ApplyRandomDebuffToRandomEnemyAction;
+import TheSecretHistories.Utils.DebugUtils;
 import TheSecretHistories.Utils.StringUtils;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
@@ -26,28 +28,7 @@ public class Grail extends AbstractPrinciple{
     public void atStartOfTurn() {
         super.atStartOfTurn();
 
-        addToBot(new AbstractGameAction() {
-            @Override
-            public void update() {
-                ArrayList<AbstractMonster> monsters = AbstractDungeon.getCurrRoom().monsters.monsters;
-                for (int i = 0; i < amount / 5; i++) {
-                    int roll = AbstractDungeon.miscRng.random(2);
-                    AbstractMonster rmo = monsters.get(new Random().nextInt(monsters.size()));
-                    switch (roll) {
-                        case 0:
-                            addToTop(new ApplyPowerAction(rmo, owner, new StrengthPower(rmo, -1)));
-                            break;
-                        case 1:
-                            addToTop(new ApplyPowerAction(rmo, owner, new WeakPower(rmo, 1, false)));
-                            break;
-                        case 2:
-                            addToTop(new ApplyPowerAction(rmo, owner, new VulnerablePower(rmo, 1, false)));
-                            break;
-                    }
-                }
-                isDone = true;
-            }
-        });
+        addToBot(new ApplyRandomDebuffToRandomEnemyAction(amount / 5, owner));
 
         this.flash();
     }
